4b29fd94e82d8833046f654d37cb2556eab1343c
vamshi <vamshi2988@gmail.com>
2019-08-29 11:45:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/postersliderapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.postersliderapp;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.ViewPager);
+
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
